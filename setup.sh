722a54f1490f8dd097b52a7441a22cf3a38f3414
keezeenavy <64618398+keezeenavy@users.noreply.github.com>
2024-11-10 07:05:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,7 @@ sudo usermod -aG docker bitwarden
 sudo mkdir /opt/bitwarden
 sudo chmod -R 700 /opt/bitwarden
 sudo chown -R bitwarden:bitwarden /opt/bitwarden
+su bitwarden
+cd /opt/bitwarden
 curl -Lso bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh
 ./bitwarden.sh install
